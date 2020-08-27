09023b2736713442abbcd1bf54b513f440b952e7
AkmElias <akmelias11@gmail.com>
2020-08-27 16:43:41 +0600

    One time final push
    -- header subheader/nav bar design
    -- results requests videocard ui/ux design
    -- tmdb api integrations
    -- fteching cetegory based data
    -- finsihing by giving project a app vibe from a website

---
+++ src/Nav.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Nav.css";
+import requests from "./requests";
+
+function Nav({ setSelectedOption }) {
+  return (
+    <div className="nav">
+      <h2 onClick={() => setSelectedOption(requests.fetchSciFyMovies)}>
+        Trending
+      </h2>
+      <h2 onClick={() => setSelectedOption(requests.fetchTopRatedMovies)}>
+        Top rated
+      </h2>
+      <h2 onClick={() => setSelectedOption(requests.fetchActionMovies)}>
+        Action
+      </h2>
+      <h2 onClick={() => setSelectedOption(requests.fetchComedyMovies)}>
+        Comedy
+      </h2>
+      <h2 onClick={() => setSelectedOption(requests.fetchHororMovies)}>
+        Horror
+      </h2>
+      <h2 onClick={() => setSelectedOption(requests.fetchRomanceMovies)}>
+        Romance
+      </h2>
+      <h2 onClick={() => setSelectedOption(requests.fetchSciFyMovies)}>
+        Sci-fy
+      </h2>
+      <h2 onClick={() => setSelectedOption(requests.fetchWesternMovies)}>
+        Western
+      </h2>
+      <h2 onClick={() => setSelectedOption(requests.fetchAnimationMovies)}>
+        Anivation
+      </h2>
+      <h2 onClick={() => setSelectedOption(requests.fetchTV)}>Movie</h2>
+    </div>
+  );
+}
+
+export default Nav;
